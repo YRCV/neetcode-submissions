646998031a26aa5f44f4e37dfc2a2d5cf265ab14
Yahil Corcino <yrc@njit.edu>
2026-06-05 00:47:41 -0400

    Add: search-2d-matrix - submission-2

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-2.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        // a matrix is just a vector of vectors (2d array)
+        // we can find the size of the vector (rows)
+        // find the size of the 0th index of that vector (columns)
+        // first find the target row: if leftCol <= target <= rightCol
+        // if so, break and we are left with midRow
+        int left = 0;
+        int right = matrix.size() - 1;
+        int midRow = (left + right) / 2;
+        int leftCol = 0;
+        int rightCol = matrix[midRow].size() - 1;
+        
+        while (left <= right){
+            midRow = (left + right) / 2;
+            if (target >= matrix[midRow][leftCol] && target <= matrix[midRow][rightCol]){
+                break;
+            } else if (matrix[midRow][leftCol] < target){
+                left = midRow + 1;
+            } else{
+                right = midRow - 1;
+            }
+        }
+
+        // perform binary search inside midRow. this is a standard array
+        while(leftCol <= rightCol){
+            int midCol = (leftCol + rightCol) / 2;
+            if (matrix[midRow][midCol] == target){
+                return true;
+            } else if (matrix[midRow][midCol] < target){
+                leftCol = midCol + 1;
+            } else {
+                rightCol = midCol - 1;
+            }
+        }
+        
+        return false;
+    }
+};
