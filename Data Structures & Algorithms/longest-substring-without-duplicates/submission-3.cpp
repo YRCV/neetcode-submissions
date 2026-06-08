0bd244ba860f9e39342efbebfaf4aa23298825e8
Yahil Corcino <yrc@njit.edu>
2026-06-08 08:48:39 -0400

    Add: longest-substring-without-duplicates - submission-3

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        // sliding window with string slicing, cleaned vers
+        // time: O(n^2), space O(n)
+        string window = "";
+        int longest = 0;
+        for (char c : s){
+            if (window.contains(c)){
+                window = window.substr(window.find(c) + 1);
+            }
+            
+            window.push_back(c);
+            longest = max(longest, (int)window.size());
+        }
+
+        return longest;
+    }
+};
